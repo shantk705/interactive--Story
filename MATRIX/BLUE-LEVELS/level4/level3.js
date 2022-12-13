63645564e656e67a45c87fcc8ae52b6415976ad8
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2022-12-13 22:11:18 +0200

    update-level4-bluepill

--- MATRIX/BLUE-LEVELS/level4/level3.js
+++ MATRIX/BLUE-LEVELS/level4/level3.js
@@ -19,10 +19,9 @@ function iShoot(enemy){
     
 
     if(!livingEnemies().length) {
-        alert(" You won! You killed the viruses that smith had sent");
+        alert(" You won! You killed the viruses that smith had sent.You saved the people of Zion and destroyed the simulation.");
     }
-    
-}
+    }
 
 
 function enemyAttacksMe(enemy){
